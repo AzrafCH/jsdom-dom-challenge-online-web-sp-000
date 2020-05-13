79565759510db816a8dc02b4f0e2df2188beb647
Azraf Chowdhury <achowdhur@ltu.edu>
2020-05-13 05:51:36 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,10 +1,8 @@
-const counter = document.getElementById('counter')
-const minus = document.getElementById('minus')
-const plus = document.getElementById('plus')
-const heart = document.getElementById('heart')
-const pause = document.getElementById('pause')
-const likes = document.getElementsByClassName('likes')
-
-function timer(seconds) {
-
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const counter = document.getElementById('counter')
+  const minus = document.getElementById('minus')
+  const plus = document.getElementById('plus')
+  const heart = document.getElementById('heart')
+  const pause = document.getElementById('pause')
+  const likes = document.getElementsByClassName('likes')
+})
