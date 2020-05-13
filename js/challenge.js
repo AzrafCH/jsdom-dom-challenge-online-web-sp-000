790ff601db53eb4b54a8c50aca8011f400eda281
Azraf Chowdhury <achowdhur@ltu.edu>
2020-05-13 05:52:19 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+  
+
+  // Dom elements
   const counter = document.getElementById('counter')
   const minus = document.getElementById('minus')
   const plus = document.getElementById('plus')
   const heart = document.getElementById('heart')
   const pause = document.getElementById('pause')
   const likes = document.getElementsByClassName('likes')
-})
+});
